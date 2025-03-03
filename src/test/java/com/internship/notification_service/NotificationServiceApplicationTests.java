22b11d9bfe609cf89c39b030697ba4b366d34c18
ArsenijeP <arsenijepetrovic763@gmail.com>
2025-03-03 15:00:08 +0100

    Setting up basic Spring stuff

---
+++ src/test/java/com/internship/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
