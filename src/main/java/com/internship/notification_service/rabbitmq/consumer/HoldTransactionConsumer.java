d3d2690cb7c03946c5f4c4bc558879fad41603a0
Stefan <stefan.segrt.0502@gmail.com>
2025-05-09 11:03:16 +0200

    Add HoldTransactionConsumer

---
+++ src/main/java/com/internship/notification_service/rabbitmq/consumer/HoldTransactionConsumer.java
@@ -0,0 +1,25 @@
+package com.internship.notification_service.rabbitmq.consumer;
+
+import com.internship.notification_service.rabbitmq.communication.Message;
+import com.internship.notification_service.rabbitmq.communication.NotificationProcessor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class HoldTransactionConsumer {
+
+    private final NotificationProcessor notificationProcessor;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queues.hold-transaction-queue}")
+    public void consumeMessage(Message message) {
+
+        notificationProcessor.saveNotification(message);
+
+        notificationProcessor.processNotification(message,
+                "Processing notification about forgot password");
+    }
+}
