6d759211716d7ee0c0fdc98184663d7191cfb0f8
Milan Stošić <milanstosic00@gmail.com>
2025-05-27 11:04:22 +0200

    Added eureka

--- src/main/java/com/internship/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/internship/notification_service/NotificationServiceApplication.java
@@ -3,12 +3,14 @@ package com.internship.notification_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRetry
 @EnableScheduling
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.internship"})
 @SpringBootApplication(scanBasePackages = {"com.internship"}, exclude = {UserDetailsServiceAutoConfiguration.class})
 public class NotificationServiceApplication {
